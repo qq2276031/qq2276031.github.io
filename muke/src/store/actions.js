84f898e12a5017c8f2b26143f5b3984c1b5adc7f
qq2276031 <524943415@qq.com>
2017-06-30 00:20:55 +0800

    “上传网站源码到github”

--- muke/src/store/actions.js
+++
@@ -1,9 +0,0 @@
-import * as types from './types'
-export default {
-     increment: ({commit}) => {commit(types.INCREMENT);},
-     clickOdd: ({commit,state}) => {
-    if (state.mutations.变量名 % 2 == 0) {   //--拿到数据做判断
-        commit(types.INCREMENT);    //输出到app.vue的mapActions
-    }
-     }
-}
